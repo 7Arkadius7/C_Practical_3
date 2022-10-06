85a4baeed4e58ab5328f7db777f0625ebe86b9ac
Sergey <motor-person@yandex.ru>
2022-10-06 21:22:56 +0400

    Решил Task_19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SecondDigit(int num)
+{
+    int firstDigit = num / 10000;
+    int secondDigit = (num / 1000) % 10;
+    int fourthDigit = (num / 10) % 10;
+    int fifthDigit = num % 10;
+
+    if (firstDigit == fifthDigit && secondDigit == fourthDigit) Console.WriteLine("Да");
+   else Console.WriteLine("Нет");
+}
+if (number > 9999 && number < 100000) SecondDigit(number);
+else Console.WriteLine("Введено не корректное число");
+
+

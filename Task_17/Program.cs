1901e5bb5e8be5466900603f88e18d848ab76521
Sergey <motor-person@yandex.ru>
2022-10-06 12:45:33 +0400

    Решили Task_18

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -7,13 +7,27 @@ Console.Write("Y: ");
 int yC = Convert.ToInt32(Console.ReadLine());
 
 int quarter = Quarter(xC, yC);
+
+string quarterStr = QuarterStr(xC, yC);
+
 Console.WriteLine(quarter == 0 ? "Введены не корректные координаты" : quarter);
 
 int Quarter(int x, int y)
 {
 if(x > 0 && y > 0) return 1;
 if(x < 0 && y > 0) return 2;
-if(x > 0 && y < 0) return 3;
-if(x < 0 && y < 0) return 4;
+if(x < 0 && y < 0) return 3;
+if(x > 0 && y < 0) return 4;
 return 0;
 }
+
+string QuarterStr(int x, int y)
+{
+if(x > 0 && y > 0) return "Первая четверть";
+else if(x < 0 && y > 0) return "Вторая четверть";
+else if(x < 0 && y < 0) return "Третья четверть";
+else if(x > 0 && y < 0) return "Четвертая четверть";
+return "Введите корректные координаты";
+}
+
+Console.WriteLine(quarterStr);
---
+++ Task_18/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string quarter = Quarter(number);
+Console.WriteLine($"Ответ: {quarter}");
+
+string Quarter(int num)
+{
+if (num == 1) return "x > 0, y > 0";
+if (num == 2) return "x < 0, y > 0";
+if (num == 3) return "x < 0, y < 0";
+if (num == 4) return "x > 0, y < 0";
+return "Введите корректную четверть";
+}

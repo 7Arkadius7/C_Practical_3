b001286f3584c91ab481120547605ddea8dd02fa
Sergey <motor-person@yandex.ru>
2022-10-06 14:04:16 +0400

    Решили Task_22

--- Task_22/Program.cs
+++ Task_22/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int number)
+{
+    int counter = 1;
+    while (counter <= number)
+    {
+        System.Console.WriteLine($"{counter} {counter * counter}");
+        counter++;
+    }
+}
+
+if (num > 0) SquareTable(num);
+else Console.WriteLine("Введено не корректное число");

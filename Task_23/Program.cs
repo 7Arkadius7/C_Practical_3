5277cfece1c42fb79103d658c27a7faa2913018f
Sergey <motor-person@yandex.ru>
2022-10-06 14:30:55 +0400

    Решили Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int number)
+{
+    int counter = 1;
+    while (counter <= number)
+    {
+        System.Console.WriteLine($"| {counter, 3} | {counter * counter * counter, 3} |");
+        counter++;
+    }
+}
+
+if (num > 0) SquareTable(num);
+else Console.WriteLine("Введено не корректное число");

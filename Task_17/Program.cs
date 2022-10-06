113fc307d77f976161ad0ef8aaa5c7354a92badc
Sergey <motor-person@yandex.ru>
2022-10-06 11:50:43 +0400

    Решили Task_17

--- Task_17/Program.cs
+++ Task_17/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите координаты точек");
+Console.Write("X: ");
+int xC = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yC = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xC, yC);
+Console.WriteLine(quarter == 0 ? "Введены не корректные координаты" : quarter);
+
+int Quarter(int x, int y)
+{
+if(x > 0 && y > 0) return 1;
+if(x < 0 && y > 0) return 2;
+if(x > 0 && y < 0) return 3;
+if(x < 0 && y < 0) return 4;
+return 0;
+}
